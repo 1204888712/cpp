56dc3300a4e7bc10ecd6a94542216ab6069ffbd9
liguobiao02 <liguobiao02@meituan.com>
2024-08-28 17:41:41 +0800

    feat: add 1.0.9

---
+++ build/Release/.deps/Release/obj.target/hello/hello.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/hello/hello.o := c++ -o Release/obj.target/hello/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=hello' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/src -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/deps/openssl/config -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/deps/openssl/openssl/include -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/deps/uv/include -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/deps/zlib -I/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/hello/hello.o.d.raw   -c
+Release/obj.target/hello/hello.o: ../hello.cc \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/node.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/cppgc/common.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8config.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-internal.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-version.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-platform.h \
+  /var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/node_version.h
+../hello.cc:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/node.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/cppgc/common.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8config.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-internal.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-version.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/v8-platform.h:
+/var/folders/3g/2kbftjj54q525d61942kw7h00000gn/T/prebuild/node/16.0.0/include/node/node_version.h:
